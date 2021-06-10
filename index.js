891d68d794d21f5b113ad04983524a06f6d989c9
jkouneski <jkouneski@yahoo.com>
2021-06-10 19:44:11 -0400

    added libraries for all roles

--- index.js
+++ index.js
@@ -4,3 +4,39 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+init = () => {
+    createManager = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Manager's name?",
+                //validate:
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Manager's ID?",
+                //validate:
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Manager's email?",
+                //validate:
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "Manager's office number?",
+                //validate:
+            }
+        ]).then(({name, id, email, officeNumber }) => {
+            const manager = new Manager(name, id, email, officeNumber);
+            console.log(manager);
+        });
+    };
+    createManager();
+};
+
+init();
