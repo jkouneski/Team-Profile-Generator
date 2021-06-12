9bc55c0d67081ee0146e4b4994499a34d2a77786
jkouneski <jkouneski@yahoo.com>
2021-06-11 23:07:30 -0400

    employee tests

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -5,3 +5,45 @@ test("Can run employee instance", () => {
     expect(typeof(e)).toBe("object")
 });
 
+test ("Can get name", () => {
+    const name = "Joey";
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+});
+
+test ("Can get id", () => {
+    const id = 1;
+    const e = new Employee("Name", id);
+    expect(e.id).toBe(id);
+});
+
+test ("Can get email", () => {
+    const email = "joey@testmail.com";
+    const e = new Employee("Name", 1, email);
+    expect(e.email).toBe(email);
+});
+
+
+test ("getName() should return Joey", () => {
+    const name = "Joey";
+    const e = new Employee(name);
+    expect(e.getName()).toEqual(name);
+});
+
+test ("getId() should return 1", () => {
+    const id = 1;
+    const e = new Employee("Name", id);
+    expect(e.getId()).toEqual(id);
+});
+
+test ("getemail() should return joey@testmail.com", () => {
+    const email = "joey@testmail.com";
+    const e = new Employee("Name", 1, "joey@testmail.com");
+    expect(e.getEmail()).toEqual(email);
+});
+
+test ("getRole() should return Employee", () => {
+    const role = "Employee";
+    const e = new Employee(role);
+    expect(e.getRole()).toEqual(role);
+});
